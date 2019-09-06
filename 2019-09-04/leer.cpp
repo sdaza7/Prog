fcc91067dbc5ca1e183520c214f86092d6b4ff5d
Samuel Alejandro Daza Rodriguez <sdaza@darkstar.org>
2019-09-06 08:45:35 -0500

    Este es mi segundo commit

---
+++ 2019-09-04/leer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+  double x = 0.0;
+  std::cout << "Escribe el numero : \n" ;
+  std::cin >> x;
+  std::cout << "He leido : " << x << "\n" ;
+  // comentario
+
+  return 0;
+}
