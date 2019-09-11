e2bc22fa30006c0cbe0e5da36841178830596049
Samuel Alejandro Daza Rodriguez <sdaza@darkstar.org>
2019-09-11 09:02:34 -0500

    asfsdf ssfsfsdf d sf sd fsf

---
+++ 2019-09-11/Variables/Exercise1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void)
+  
+{
+  double miles = 0.0;
+  double km = 0.0;
+  std::cout <<"Ingrese el Numero de Millas que desea Convertir :\n ";
+  std::cin >> miles;
+  double result=  km= miles*1.609;
+  std::cout << "El resultado es:"<<km; 
+ 
+    
+
+  return 0;
+}
