0e4c804e764d2ef481ea91f23f8a65fdb5ec6d44
Samuel Alejandro Daza Rodriguez <sdaza@darkstar.org>
2019-09-13 09:43:14 -0500

    Este es el Programa que al Escribir un nombre, imprime un saludo

---
+++ 2019-09-13/Taller/hola.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+  std::string x;
+  std::cout << "Escriba su Nombre: \n";
+  std::cin >> x;
+  std::cout << "Hola," << x << "! \n";
+  
+
+
+  return 0;
+}
