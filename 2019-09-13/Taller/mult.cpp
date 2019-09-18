b45c0f57b8320195450da71d27f586b2da082ddc
Samuel Alejandro Daza Rodriguez <sdaza@darkstar.org>
2019-09-18 08:45:41 -0500

    Este programa permite duplicar el numero que se ingrese

---
+++ 2019-09-13/Taller/mult.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(void)
+{
+  double x = 0.0;
+  //Se ingresa el número y se presiona enter
+  std::cout << "Ingrese el número que desee Duplicar: \n";
+  std::cin >> x;
+  //Se define una Variable Y correspondiente al doble del Número ingresado
+  double y = 2*x;
+  std::cout << "El Resultado es: " << y << "\n";
+  
+  
+
+
+
+
+  return 0;
+}
